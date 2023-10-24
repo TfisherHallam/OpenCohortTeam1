6b259829b6bbfcf8c135305ae782c412ac81c43c
Tom Fisher <thomas.fisher@theprivateoffice.com>
2023-10-24 18:43:46 +0100

    Search bar added, requires linking to Mern DB

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import { SearchBar } from './components/SearchBar/SearchBar';
 import Header from './components/Header/header.js';
+import { useState } from 'react';
+import { SearchResultsList } from './components/SearchBar/SearchResultsList';
 
 function App() {
+
+  const [results, setResults] = useState([]);
   return (
     <div className="App">
-      <Header/>
-      <SearchBar/>
+
+      <SearchBar setResults={setResults}/>
+      <SearchResultsList results={results}/>
     </div>
   );
 }
