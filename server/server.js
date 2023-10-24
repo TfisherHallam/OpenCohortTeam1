bca8b76fe96c072b45ded6f9f79fbd80ecac1508
Tom Fisher <thomas.fisher@theprivateoffice.com>
2023-10-24 10:54:33 +0100

    Updates to adding some middleware for auth

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
 const app = express();
 const port = process.env.PORT || 3000;
 const routes = require('./routes/routes.js');
@@ -8,4 +11,21 @@ app.use('/', routes);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-
+// Bodyparser middleware
+app.use(
+    bodyParser.urlencoded({
+      extended: false
+    })
+  );
+  app.use(bodyParser.json());
+  // DB Config
+  const db = require("./config/keys").mongoURI;
+  // Connect to MongoDB
+  mongoose
+    .connect(
+      db,
+      { useNewUrlParser: true }
+    )
+    .then(() => console.log("MongoDB successfully connected"))
+    .catch(err => console.log(err));
+  app.listen(port, () => console.log(`Server up and running on port ${port} !`));
