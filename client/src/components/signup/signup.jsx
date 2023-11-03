f44630d9119fe21733218eb220cbfb702fbbc914
Tom Fisher <thomas.fisher@theprivateoffice.com>
2023-11-03 13:48:29 +0000

    Need to add routes

---
+++ client/src/components/signup/signup.jsx
@@ -0,0 +1,105 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from axios;
+import styles from './signupstyles.module.css';
+
+const Signup = () => {
+    const [data, setData] = useState({
+        firstName: "",
+        lastName: "",
+        userName: "",
+        email: "",
+        password: ""
+    });
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+    const handleChange = ({ currentTarget: input }) => {
+        setData({ ...data, [input.name]: input.value });
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const url = "http://localhost:8080/api/users";
+            const { data: res } = await axios.post(url, data);
+            navigate("/login")
+            console.log(res.message);
+        } catch (error) {
+            if (error.response &&
+                error.response.status >= 400 &&
+                error.response.status <= 500) {
+                setError(error.response.data.message)
+            }
+        }
+    }
+
+    return (
+        <div className={styles.Signup_container}>
+            <div className={styles.signup_form_container}>
+                <div className={styles.left}>
+                    <h1>Welcome back to TicketScalper</h1>
+                    <link to="/login">
+                        <button type='button' className={styles.white_btn}>
+                            Sign in
+                        </button>
+                    </link>
+                </div>
+                <div className={styles.right}></div>
+                <form className={styles.form_container} onSubmit={handleSubmit}>
+                    <h1>Register</h1>
+                    <input
+                        type="text"
+                        placeholder='First Name'
+                        name='firstName'
+                        onChange={handleChange}
+                        value={data.firstName}
+                        required
+                        className={styles.input}
+                    />
+                    <input
+                        type="text"
+                        placeholder='Last Name'
+                        name='lastName'
+                        onChange={handleChange}
+                        value={data.lastName}
+                        required
+                        className={styles.input}
+                    />
+                    <input
+                        type="text"
+                        placeholder='Username'
+                        name='userName'
+                        onChange={handleChange}
+                        value={data.userName}
+                        required
+                        className={styles.input}
+                    />
+                    <input
+                        type="email"
+                        placeholder='Email'
+                        name='email'
+                        onChange={handleChange}
+                        value={data.email}
+                        required
+                        className={styles.input}
+                    />
+                    <input
+                        type="password"
+                        placeholder='Password'
+                        name='password'
+                        onChange={handleChange}
+                        value={data.password}
+                        required
+                        className={styles.input}
+                    />
+                    {error && <div className={styles.error_msg}>{error}</div>}
+                    <button type="submit" className={styles.green_btn}>
+                        Register
+                    </button>
+                </form>
+            </div>
+        </div>
+    )
+};
+
+export default Signup;
