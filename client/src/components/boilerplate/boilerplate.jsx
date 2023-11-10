1913a11daa6b5f393c18cc3ee62d103cf153fbfe
Rhydian Downing <Rhydian.Downing@tui.co.uk>
2023-11-10 16:34:30 +0000

    added burger back to header

--- client/src/components/boilerplate/boilerplate.jsx
+++ client/src/components/boilerplate/boilerplate.jsx
@@ -9,7 +9,7 @@ function Boilerplate() {
         <div class="flex-container">
             <div class="aboutFlex">
                 <div><b>About TicketScalper</b></div>
-                <div><Link to={"/Termsofuse"}>Terms of Use</Link></div>
+                <span><Link to={"/Termsofuse"}>Terms of Use</Link></span>
                 <div><Link to={"/Privacy"}>Privacy</Link></div>
             </div>
             <div class="aboutFlex">
