d0a01b665efbcebcab942f64bdea5409e75e34b4
Tom Fisher <thomas.fisher@theprivateoffice.com>
2023-11-12 08:34:10 +0000

    Updates

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 import express from "express";
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb+srv://c3060912:cultsolveallbeckham@scalperdb.fgvbnpd.mongodb.net/?retryWrites=true&w=majority")
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Running on 3000');
+    console.log('Running on 3001');
 }
 )
--- server/routes/routes.js
+++
@@ -1,7 +0,0 @@
-﻿const express = require('express');
-const router =  express.Router();
-
-router.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-module.exports = router;
