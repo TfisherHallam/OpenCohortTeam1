2c2126a2877bc3492691bd8e8fff8f5b77c3ab89
Rhydian Downing <c3068418@hallam.shu.ac.uk>
2023-11-17 13:42:42 +0000

    added new burger menu

--- client/src/components/Header/header.jsx
+++ client/src/components/Header/header.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import './header.css';
 import logo from "../../images/TransparentLogo.png";
-import BurgerMenu from "../burgerNavigation/burgerMenu.js";
+import BurgerNew from "../burgerNavigation/hamBurgerMenu.jsx";
 
 function Logo() {
-    return (
-        <div>
-            <a href={"/"}><img src={logo} alt="Ticket Scalper Logo" class={"headerLogo"} /></a>
-        </div>
-    )
+  return (<div>
+    <a href={"/"}><img src={logo} alt="Ticket Scalper Logo"
+                       className={"headerLogo"}/></a>
+  </div>)
 }
 
 function Header() {
-    return (
-        <div className={"appHeader"}>
-            <Logo />
-           
-        </div>
-    )
+  return (<div className={"appHeader"}>
+    <Logo/>,
+    <BurgerNew/>,
+
+  </div>)
 }
 
 export default Header;
---
+++ client/src/components/burgerNavigation/hamBurgerMenu.jsx
@@ -0,0 +1,17 @@
+import { slide as Menu } from 'react-burger-menu';
+import React from "react";
+import './hamBurgerMenu.css';
+
+
+function BurgerNew () {
+    return (
+        <Menu>
+          <a id="/Home" className="menu-item" href="/">Home</a>
+          <a id="/Listing" className="menu-item" href="/Listing">Listing</a>
+          <a id="/Buying" className="menu-item" href="/Buying">Buying</a>
+          <a id="/Account" className="menu-item" href="/Account">Account</a>
+        </Menu>
+    );
+  }
+
+export default BurgerNew;
