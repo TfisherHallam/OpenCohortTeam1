9dc2401bd2c870e378279d3728a96e153ee24190
Tom Fisher <thomas.fisher@theprivateoffice.com>
2023-11-18 15:34:49 +0000

    Login with JWT works correctly

--- client/src/components/login/login.jsx
+++ client/src/components/login/login.jsx
@@ -1,80 +1,88 @@
-import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-  signInStart,
-  signInSuccess,
-  signInFailure,
-} from '../redux/user/userSlice';
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { FaUserCircle } from 'react-icons/fa';
+import './login.css';
+import '../../App.css';
 
-export default function SignIn() {
-  const [formData, setFormData] = useState({});
-  const { loading, error } = useSelector((state) => state.user);
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.id]: e.target.value,
-    });
-  };
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      dispatch(signInStart());
-      const res = await fetch('/api/auth/signin', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
-      const data = await res.json();
-      console.log(data);
-      if (data.success === false) {
-        dispatch(signInFailure(data.message));
-        return;
-      }
-      dispatch(signInSuccess(data));
-      navigate('/');
-    } catch (error) {
-      dispatch(signInFailure(error.message));
-    }
-  };
-  return (
-    <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
-      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        <input
-          type='email'
-          placeholder='email'
-          className='border p-3 rounded-lg'
-          id='email'
-          onChange={handleChange}
-        />
-        <input
-          type='password'
-          placeholder='password'
-          className='border p-3 rounded-lg'
-          id='password'
-          onChange={handleChange}
-        />
+export default function Login() {
+	const [formData, setFormData] = useState({});
+	const [error, setError] = useState(null);
+	const [loading, setLoading] = useState(false);
+	const navigate = useNavigate();
+	const handleChange = (e) => {
+		setFormData({
+			...formData,
+			[e.target.id]: e.target.value,
+		});
+	};
 
-        <button
-          disabled={loading}
-          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
-        >
-          {loading ? 'Loading...' : 'Sign In'}
-        </button>
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			setLoading(true);
+			const res = await fetch('/api/auth/signin', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(formData),
+			});
+			const data = await res.json();
+			console.log(data);
+			if (data.success === false) {
+				setLoading(false);
+				setError(data.message);
+				return;
+			}
+			setLoading(false);
+			setError(null);
+			navigate('/');
+		} catch (error) {
+			setLoading(false);
+			setError(error.message);
+		}
+	};
 
-      </form>
-      <div className='flex gap-2 mt-5'>
-        <p>Dont have an account?</p>
-        <Link to={'/sign-up'}>
-          <span className='text-blue-700'>Sign up</span>
-        </Link>
-      </div>
-      {error && <p className='text-red-500 mt-5'>{error}</p>}
-    </div>
-  );
-}
+	return (
+<div class='textflex-container'>
+			<div class='textcontainer'>
+				<div>
+					<form  class='loginform' onSubmit={handleSubmit}>
+						<h1>Welcome back</h1>
+						<br/>
+						<p><Link to={"/Register"}>New user? Sign up here</Link></p>
+						<br/>						
+						<FaUserCircle class='icon' size={70}/>
+						<br/>
+						<br/>
+						<input
+						type="email"
+						placeholder="Email"
+						id="email"
+						onChange={handleChange}
+						required
+						className="inputboxes"
+					/>
+						<br/>
+						<br/>
+						<input
+						type="password"
+						placeholder="Password"
+						id="password"
+						onChange={handleChange}
+						required
+						className="inputboxes"
+					/>
+						{error && <div class="">{error}</div>}
+						<br/>
+						<br/>
+						<button type="submit" class="loginbutton">
+							SIGN IN
+						</button>
+					</form>
+					<p><Link to={"/"}>Forgotten your password? Click here to reset</Link></p>
+				</div>				
+			</div>
+		</div>
+);
+};
