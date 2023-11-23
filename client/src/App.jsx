e58a302abcba83f582a3dfe9b306219a7f77fdcc
Tom Fisher <thomas.fisher@theprivateoffice.com>
2023-11-23 16:57:20 +0000

    Redux persist added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   return (
     <>
       <div className="App">
-        <Header />
+        <Header />,
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/Listing' element={<ListingPage />} />
