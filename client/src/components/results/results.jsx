a20f2faab6e9232227241fc16f943369af17ff20
Rhydian Downing <c3068418@hallam.shu.ac.uk>
2023-11-29 01:00:35 +0000

    added css for buying pages

--- client/src/components/results/results.jsx
+++ client/src/components/results/results.jsx
@@ -1,5 +1,5 @@
 import './results.css';
-import {useEffect, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 
 const PORT = process.env.PORT || 3001;
 
@@ -45,6 +45,8 @@ function SearchResults() {
     startDate: null,
     endDate: null,
   });
+  const [isFilterOpen, setIsFilterOpen] = useState(false);
+  const filterSectionRef = useRef(null);
 
   useEffect(() => {
     async function fetchData() {
@@ -79,7 +81,6 @@ function SearchResults() {
     fetchData();
   }, [bidRange, selectedDateRange, selectedEventType]);
 
-
   useEffect(() => {
     // Update countdown every second
     const intervalId = setInterval(() => {
@@ -99,7 +100,6 @@ function SearchResults() {
       [dateType]: dateValue ? new Date(dateValue) : null,
     }));
   };
-
 
   const sortOptions = {
     lowestCountdown: (a, b) => {
@@ -135,27 +135,46 @@ function SearchResults() {
         result.currentBid >= bidRange[0] && result.currentBid <= bidRange[1];
 
     const isDateInRange =
-        (!selectedDateRange.startDate || new Date(result.eventDate) >= selectedDateRange.startDate) &&
-        (!selectedDateRange.endDate || new Date(result.eventDate) <= selectedDateRange.endDate);
+        (!selectedDateRange.startDate || new Date(result.eventDate)
+            >= selectedDateRange.startDate) &&
+        (!selectedDateRange.endDate || new Date(result.eventDate)
+            <= selectedDateRange.endDate);
 
     const isEventTypeMatch =
         selectedEventType === '' || result.eventType === selectedEventType;
 
-    return isNameDescriptionMatch && isBidInRange && isDateInRange && isEventTypeMatch;
+    return isNameDescriptionMatch && isBidInRange && isDateInRange
+        && isEventTypeMatch;
   })
   .sort(sortOptions[sortOption]);
 
+  useEffect(() => {
+    filterSectionRef.current?.style &&
+    (filterSectionRef.current.style.maxHeight = isFilterOpen
+        ? filterSectionRef.current.scrollHeight + 'px'
+        : '0');
+  }, [isFilterOpen]);
 
   return (<div className="Results">
-        <div className="SearchAndSort">
-          <input
-              type="text"
-              placeholder="Search..."
-              value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)}
-          />
-        </div>
-        <div className="SearchAndSort">
+
+    <div className="SearchAndSort SearchMain">
+      <input
+          type="text"
+          placeholder="Search..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+      />
+    </div>
+    <div className="SearchAndSort">
+      <button
+          className="toggleFilterButton"
+          onClick={() => setIsFilterOpen(!isFilterOpen)}
+      >
+        {isFilterOpen ? 'Hide Filters' : 'Show Filters'}
+      </button>
+      <div className={`filterContainer ${isFilterOpen ? 'open' : 'closed'}`}>
+        <div className="filterSection First" ref={filterSectionRef}>
+
           <label htmlFor="sortOption">Sort by:</label>
           <select
               id="sortOption"
@@ -167,99 +186,120 @@ function SearchResults() {
             <option value="highestPrice">Highest Price</option>
             <option value="lowestPrice">Lowest Price</option>
           </select>
-          <select
-              value={selectedEventType}
-              onChange={(e) => setSelectedEventType(e.target.value)}
-          >
-            <option value="">All Event Types</option>
-            <option value="Gig">Gig</option>
-            <option value="Concert">Concert</option>
-            <option value="Festival">Festival</option>
-            <option value="Comedy Night">Comedy Night</option>
-            {/* Add more event types based on your data */}
-          </select>
-          <div className="BidFilter">
-            <label>Price Range:</label>
-            <input
-                type="number"
-                min="0"
-                max="1000"
-                step="10"
-                value={bidRange[0]}
-                onChange={(e) => setBidRange([parseInt(e.target.value), bidRange[1]])}
-            />
-            <input
-                type="number"
-                min="0"
-                max="1000"
-                step="10"
-                value={bidRange[1]}
-                onChange={(e) => setBidRange([bidRange[0], parseInt(e.target.value)])}
-            />
-            <p>{`£${bidRange[0]} - £${bidRange[1]}`}</p>
-          </div>
-          <div className="DateRange">
-            <label>Start Date:</label>
-            <input
-                type="date"
-                value={
-                  selectedDateRange.startDate
-                      ? selectedDateRange.startDate.toISOString().split('T')[0]
-                      : ''
-                }
-                onChange={(e) => handleDateChange('startDate', e.target.value)}
-            />
-            <label>End Date:</label>
-            <input
-                type="date"
-                value={
-                  selectedDateRange.endDate
-                      ? selectedDateRange.endDate.toISOString().split('T')[0]
-                      : ''
-                }
-                onChange={(e) => handleDateChange('endDate', e.target.value)}
-            />
-          </div>
+          <label htmlFor="eventType">Event Type:
+            <select
+                value={selectedEventType}
+                onChange={(e) => setSelectedEventType(e.target.value)}
+            >
+              <option value="">All Event Types</option>
+              <option value="Gig">Gig</option>
+              <option value="Concert">Concert</option>
+              <option value="Festival">Festival</option>
+              <option value="Comedy Night">Comedy Night</option>
+            </select></label>
+          <div className="filterSection Second" ref={filterSectionRef}>
+            <div className="BidFilter">
+              <label>Price Range:
+                <div></div>
+                <label>Min:
+                  <input
+                      type="number"
+                      min="0"
+                      max="1000"
+                      step="10"
+                      value={bidRange[0]}
+                      onChange={(e) => setBidRange(
+                          [parseInt(e.target.value), bidRange[1]])}
+                  />
+                </label>
+                <label>Max:
+                  <input
+                      type="number"
+                      min="0"
+                      max="1000"
+                      step="10"
+                      value={bidRange[1]}
+                      onChange={(e) => setBidRange(
+                          [bidRange[0], parseInt(e.target.value)])}
+                  />
+                </label>
+              </label>
+            </div>
+            <p style={{
+              textAlign: 'center',
+              fontWeight: 'bold'
+            }}>{`£${bidRange[0]} - £${bidRange[1]}`}</p>
+            <div className="DateRange">
 
-        </div>
-        <div className="Container">
-          <div className="Carousel">
-            {filteredAndSortedResults && filteredAndSortedResults.length > 0
-                ? (filteredAndSortedResults.map((result) => (
-                        <a key={result._id} href={`/itemView/${result._id}`}
-                           className="AuctionItem">
-                          <img src={result.image} alt={result.eventName}
-                               className="AuctionImage"/>
-                          <div key={result.id} className="AuctionDetails">
-                            <h2 className={"eventTitle"}>{result.eventName}</h2>
-                            <p style={{textAlign: 'right'}}>Date: {new Date(
-                                result.eventDate).toLocaleDateString()}  </p>
-                            <p style={{textAlign: 'right'}}>Time: {formatTime(
-                                result.eventTime)}</p>
-                            <p style={{textAlign: 'right'}}>Sold
-                              By: {result.username}</p>
-                            <p style={{textAlign: 'left'}}> {result.description}</p>
-                            <div className="line"></div>
-                            <div className="bidAndCountdown">
-                              <p className="CurrentBid">Current Bid:
-                                £{result.currentBid}</p>
-                              <div className="Countdown">
-                                <p>{calculateCountdown(result.auctionEndDate,
-                                    result.auctionEndTime)}</p>
-                              </div>
-                            </div>
-                            <p className={"auctionEnd"}>Auction End: {new Date(
-                                result.auctionEndDate).toLocaleDateString()} at {formatTime(
-                                result.auctionEndTime)}</p>
-                          </div>
-
-                        </a>
-
-                    ))) : (// Display a message when no results are available
-                    <p>No auction listings found.</p>)}
+              <label> Event Date Range: {`\n`}
+                <label>Start Date:
+                  <input
+                      type="date"
+                      value={
+                        selectedDateRange.startDate
+                            ? selectedDateRange.startDate.toISOString().split(
+                                'T')[0]
+                            : ''
+                      }
+                      onChange={(e) => handleDateChange('startDate',
+                          e.target.value)}
+                  /></label>
+                <label>End Date:
+                  <input
+                      type="date"
+                      value={
+                        selectedDateRange.endDate
+                            ? selectedDateRange.endDate.toISOString().split(
+                                'T')[0]
+                            : ''
+                      }
+                      onChange={(e) => handleDateChange('endDate',
+                          e.target.value)}
+                  /></label></label></div>
           </div>
         </div>
-      </div>);
+      </div>
+
+    </div>
+    <div className="Container">
+      <div className="Carousel">
+        {filteredAndSortedResults && filteredAndSortedResults.length > 0
+            ? (filteredAndSortedResults.map((result) => (
+                <a key={result._id} href={`/itemView/${result._id}`}
+                   className="AuctionItem">
+                  <img src={result.image} alt={result.eventName}
+                       className="AuctionImage"/>
+                  <div key={result.id} className="AuctionDetails">
+                    <h2 className={"eventTitle"}>{result.eventName}</h2>
+                    <p style={{textAlign: 'right'}}>Date: {new Date(
+                        result.eventDate).toLocaleDateString()}  </p>
+                    <p style={{textAlign: 'right'}}>Time: {formatTime(
+                        result.eventTime)}</p>
+                    <p style={{textAlign: 'right'}}>Sold
+                      By: {result.username}</p>
+                    <p style={{textAlign: 'left'}}> {result.description}</p>
+                    <div className="line"></div>
+                    <div className="bidAndCountdown">
+                      <p className="CurrentBid">Current Bid:
+                        £{result.currentBid}</p>
+                      <div className="Countdown">
+                        <p>{calculateCountdown(result.auctionEndDate,
+                            result.auctionEndTime)}</p>
+                      </div>
+                    </div>
+                    <p className={"auctionEnd"}>Auction End: {new Date(
+                        result.auctionEndDate).toLocaleDateString()} at {formatTime(
+                        result.auctionEndTime)}</p>
+                  </div>
+
+                </a>
+
+            ))) : (// Display a message when no results are available
+                <p>No auction listings found. Try changing your search
+                  criteria! </p>)}
+      </div>
+    </div>
+  </div>);
 }
 
 export default SearchResults;
